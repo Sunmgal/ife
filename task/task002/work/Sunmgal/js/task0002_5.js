196c1d33fe7dbe89390c334eb7af8aa59c66866d
Sunmgal <skq110@qq.com>
2015-04-28 17:32:56 +0800

    Update task0002_5.js
    还在继续完善修改中

--- task/task002/work/Sunmgal/js/task0002_5.js
+++ task/task002/work/Sunmgal/js/task0002_5.js
@@ -1,52 +1,63 @@
 window.onload = function(){	
 		var oUl1 = document.getElementById("ul1");
 		var oUl2 = document.getElementById("ul2");
-		var aLi1 = oUl1.getElementsByTagName("li")[0];
-		var aLi2 = oUl2.getElementsByTagName("li")[0];
+		var aLi1 = oUl1.getElementsByTagName("li");
+		var aLi2 = oUl2.getElementsByTagName("li");
  
+		//设置间距
+		function setStyle(obj){
+			var iHeight = 50;
+			for(var i=0;i<obj.length;i++){
+				obj[i].style.top = i*iHeight + 'px';
+			}
+		}
+		setStyle(aLi1);
+		setStyle(aLi2);
+
  		function move(obj,i){
 			obj.onmousedown = function(ev){
 				var ev = ev || event;
-				var X = ev.clientX - obj.offsetLeft;
-				var Y = ev.clientY - obj.offsetTop;
+				var target = ev.srcElement || ev.target;
+				var X = ev.clientX - target.offsetLeft;
+				var Y = ev.clientY - target.offsetTop;
 
 				//在非标准浏览器下设置全局捕获
-				if(obj.setCapture){
-					obj.setCapture();   
+				if(target.setCapture){
+					target.setCapture();   
 				}
 
-				document.onmousemove = function(ev){ 
-					var ev = ev || event;
-					//设置obj移动的范围
-					var L = ev.clientX - X;
-					var T = ev.clientY - Y 
-					if(L*i<0){
-						L = 0;
-					}else if(L*i > 60){
-						L = i*(oUl2.clientLeft + oUl2.offsetWidth + 60);
-					}
+				if(target.nodeName.toLowerCase() === 'li'){  //用nodeName排除ul
+					document.onmousemove = function(ev){ 
+						var ev = ev || event;
+						//设置obj移动的范围
+						var L = ev.clientX - X;
+						var T = ev.clientY - Y 
+						if(L*i<0){
+							L = 0;
+						}else if(L*i > 59){
+							L = i*(obj.clientLeft + obj.offsetWidth + 59);
+						}
 
-					if(T<0){
-						T = 0;
-					}else if(T>oUl2.offsetHeight){
-						T = oUl2.offsetHeight - obj.offsetHeight;
-					} 
+						if(T<0){
+							T = 0;
+						}else if(T>obj.offsetHeight - target.offsetHeight){
+							T = obj.offsetHeight - target.offsetHeight ;
+						} 
 
+						target.style.left = L + 'px';
+						target.style.top = T + 'px';	
+					};
 
-					obj.style.left = L + 'px';
-					obj.style.top = T + 'px';	
-				};
-
-				document.onmouseup = function(){
-					document.onmousemove = document.onmouseup = null;
-					if(obj.releaseCapture){
-						obj.releaseCapture();   
-					}					
-				};
-
+					document.onmouseup = function(){
+						document.onmousemove = document.onmouseup = null;
+						if(target.releaseCapture){
+							target.releaseCapture();   
+						}					
+					};
+				}	
 				return false;	
 			}
 		}
-		move(aLi1,1);
-		move(aLi2,-1);		
+		move(oUl1,1);
+		move(oUl2,-1);		
 	};
